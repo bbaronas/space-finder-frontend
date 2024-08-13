a3929f41444d2550e003906b6b3e720a8b3fbf86
Barry Baronas <barry@bithoop.com>
2024-08-13 09:08:23 -0400

    establish frontend

---
+++ src/services/AuthService.ts
@@ -0,0 +1,80 @@
+import { Amplify } from "aws-amplify";
+import { SignInOutput, fetchAuthSession, signIn } from '@aws-amplify/auth';
+import { AuthStack } from '../../../space-finder/outputs.json';
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
+import { AwsCredentialIdentity } from "@aws-sdk/types";
+
+const awsRegion = 'us-east-1';
+
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolId: AuthStack.SpaceUserPoolId,
+            userPoolClientId: AuthStack.SpaceUserPoolClientId,
+            identityPoolId: AuthStack.SpaceIdentityPoolId,
+        }
+    }
+});
+
+export class AuthService {
+
+    private user: SignInOutput | undefined;
+    private userName: string = '';
+    private jwtToken: string | undefined;
+    private temporaryCredentials: AwsCredentialIdentity | undefined;
+
+    public async login(userName: string, password: string): Promise<object | undefined> {
+        try {
+            const signInOutput: SignInOutput = await signIn({
+                username: userName,
+                password: password,
+                options: {
+                    authFlowType: 'USER_PASSWORD_AUTH'
+                }
+            });
+            this.user = signInOutput;
+            this.userName = userName;
+            await this.generateIdToken();
+            return this.user;
+        } catch (error) {
+            console.error(error);
+            return undefined;
+        }
+    }
+
+    private async generateIdToken() {
+        this.jwtToken = (await fetchAuthSession()).tokens?.idToken?.toString();
+    }
+
+    public async getTemporaryCredentials() {
+        if (this.temporaryCredentials) {
+            return this.temporaryCredentials;
+        }
+       this.temporaryCredentials = await this.generateTempCredentials();
+       return this.temporaryCredentials;
+    }
+
+    private async generateTempCredentials(): Promise<AwsCredentialIdentity> {
+        const cognitoIdentityPool = `cognito-idp.${awsRegion}.amazonaws.com/${AuthStack.SpaceIdentityPoolId}`;
+        const cognitoIdentity = new CognitoIdentityClient({
+            credentials: fromCognitoIdentityPool({
+                clientConfig: { region: awsRegion },
+                identityPoolId: AuthStack.SpaceIdentityPoolId,
+                logins: {
+                    [cognitoIdentityPool]: this.jwtToken!
+                }
+            })
+        });
+        const credentials = await cognitoIdentity.config.credentials();
+        return credentials;
+    }
+
+    public getIdToken() {
+        return this.jwtToken;
+    }
+
+    public getUserName() {
+        return this.userName;
+    }
+}
---
+++ src/services/DataService.ts
@@ -0,0 +1,48 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { AuthService } from "./AuthService";
+import { DataStack } from "../../../space-finder/outputs.json";
+
+
+
+
+export class DataService {
+
+    private authService: AuthService;
+    private s3Client: S3Client | undefined;
+    private awsRegion = 'us-east-1';
+
+    constructor(authService: AuthService) {
+        this.authService = authService;
+    }
+
+    public async createSpace(name: string, location:string, photo?: File) {
+        if (photo) {
+            const uploadUrl = await this.uploadPublicFile(photo)
+            console.log(uploadUrl);
+        };
+
+        return '123';
+    }
+
+    private async uploadPublicFile(file: File) {
+        const credentials = await this.authService.getTemporaryCredentials();
+        if (!this.s3Client) {
+            this.s3Client = new S3Client({
+                credentials,
+                region: this.awsRegion
+            });
+        }
+        const command = new PutObjectCommand({
+            Bucket: DataStack.SpaceFinderPhotosBucketName,
+            Key: file.name,
+            ACL: 'public-read',
+            Body: file
+        })
+        await this.s3Client.send(command);
+        return `https://${command.input.Bucket}.s3.${this.awsRegion}.amazonaws.com/${command.input.Key}`;
+    }
+
+    public isAuthorized() {
+        return true;
+    }
+}
